6183476cc713cc5a9302b7f0287155d5a4619d72
FacundoAguilar19 <Facundoalbertoaguilar@gmail.com>
2023-05-28 20:56:32 -0300

    Update contacto.js

--- contacto.js
+++ contacto.js
@@ -11,14 +11,13 @@ form.addEventListener("submit",function(evento){
  evento.preventDefault();
  var errores=[]
  if(nombre.value ===null || nombre.value ===''){
-  errores.push("ingrese su nombre")
+  errores.push("Ingrese su nombre")
  }
  
-
- if(telefono.value ===null || telefono.value ===''){
-  errores.push("ingrese tu telefono")
- }
-
+ if (telefono.value === null || telefono.value === ''||telefono.value.length > 10) {
+    errores.push("Ingrese su teléfono correctamente");
+    errores.push("El teléfono debe tener como máximo 10 caracteres");
+  }
  if(email.value ===null || email.value ===''){
   errores.push("ingresa tu email")
  }
