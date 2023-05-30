b21063a3f435027be9aa9faf06372ceef94bc3b4
FacundoAguilar19 <Facundoalbertoaguilar@gmail.com>
2023-05-30 13:16:22 -0300

    v2

--- contacto.js
+++ contacto.js
@@ -10,27 +10,37 @@ form.addEventListener("submit",function(evento){
 
  evento.preventDefault();
  var errores=[]
- if(nombre.value ===null || nombre.value ===''){
+ if(nombre.value ===''){
   errores.push("Ingrese su nombre")
  }
  
- if (telefono.value === null || telefono.value === ''||telefono.value.length > 10) {
+ if (telefono.value === '' || isNaN(telefono.value) || telefono.value.length !== 10) {
     errores.push("Ingrese su teléfono correctamente");
-    errores.push("El teléfono debe tener como máximo 10 caracteres");
+    errores.push("El teléfono debe tener 10 caracteres");
   }
- if(email.value ===null || email.value ===''){
-  errores.push("ingresa tu email")
- }
+  if (email.value === '' || !validarEmail(email.value)) {
+    errores.push("Ingrese su correo electrónico correctamente");
+  }
   
-  if(mensaje.value ===null || mensaje.value ===''){
+  if(mensaje.value ===''){
       errores.push("ingresa tu mensaje");
   }
-  
+  if (errores.length === 0) {
+
+    nombre.value = '';
+    telefono.value = '';
+    email.value = '';
+    mensaje.value = '';
+    console.log("Formulario enviado correctamente")
+  }
 
 error.innerHTML=errores.join(', ');
 
 });
-
+function validarEmail(email) {
+  var re = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/;
+  return re.test(email);
+}
 
 
 
