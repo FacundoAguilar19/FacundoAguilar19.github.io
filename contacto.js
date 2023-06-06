7f48893716dd31a5adf4995bd9d0c092bfd1ee80
FacundoAguilar19 <Facundoalbertoaguilar@gmail.com>
2023-06-06 02:36:34 -0300

    intento 1 de mostrar datos del usuario

--- contacto.js
+++ contacto.js
@@ -2,30 +2,48 @@
 var nombre = document.getElementById('name');
 var telefono = document.getElementById('phone');
 var email = document.getElementById('email');
-var  mensaje = document.getElementById('message');
-error.style.color="orange";
+var mensaje = document.getElementById('message');
+error.style.color = "orange";
 
-var form= document.getElementById("formulario");
-form.addEventListener("submit",function(evento){
 
- evento.preventDefault();
- var errores=[]
- if(nombre.value ===''){
-  errores.push("Ingrese su nombre")
- }
- 
- if (telefono.value === '' || isNaN(telefono.value) || telefono.value.length !== 10) {
+var form = document.getElementById("formulario");
+var datosRecibidos = [];
+form.addEventListener("submit", function (evento) {
+
+  evento.preventDefault();
+  var errores = []
+  if (nombre.value === '') {
+    errores.push("Ingrese su nombre")
+  }
+
+  if (telefono.value === '' || isNaN(telefono.value) || telefono.value.length !== 10) {
     errores.push("Ingrese su teléfono correctamente");
     errores.push("El teléfono debe tener 10 numeros");
   }
   if (email.value === '' || !validarEmail(email.value)) {
     errores.push("Ingrese su correo electrónico correctamente");
   }
-  
-  if(mensaje.value ===''){
-      errores.push("ingresa tu mensaje");
+
+  if (mensaje.value === '') {
+    errores.push("ingresa tu mensaje");
   }
   if (errores.length === 0) {
+    var datos = {
+      nombre: nombre.value,
+      telefono: telefono.value,
+      email: email.value,
+      mensaje: mensaje.value
+    };
+    datosRecibidos.push(datos);
+    var datosIngresadosDiv = document.getElementById('datosIngresados');
+    var datosIngresados = [];
+    for (var i = 0; i < datosRecibidos.length; i++) {
+      var dato = datosRecibidos[i];
+      var datosEnLinea = `${dato.nombre}, ${dato.telefono}, ${dato.email}, ${dato.mensaje}`;
+      datosIngresados.push(datosEnLinea);
+    }
+    datosIngresadosDiv.textContent = datosIngresados.join(',<br> ');
+    datosIngresadosDiv.style.color = "orange";
 
     nombre.value = '';
     telefono.value = '';
@@ -33,14 +51,15 @@ form.addEventListener("submit",function(evento){
     mensaje.value = '';
     console.log("Formulario enviado correctamente")
   }
-
-error.innerHTML=errores.join(', ');
+ 
+  error.innerHTML = errores.join(', ');
 
 });
 function validarEmail(email) {
   var re = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/;
   return re.test(email);
 }
+
 
 
 
